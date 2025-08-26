2aab88193852a76649cddbfbd828015d7eb89e8b
Bilal17809 <khanbangash17809@gmail.com>
2025-08-26 16:06:00 +0500

    by

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import '/pages/splash/view/splash_view.dart';
+import 'core/dependency_inject/dependency_inject.dart';
+
+void main() {
+  // DependencyInject.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
